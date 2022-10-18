bdef3c0e2cbd6d433f61a6efd7abbc1c21e3fe71
Marcos Bruno <marcosferreyra82@gmail.com>
2022-10-17 21:40:23 -0300

    Let,if,elseif

---
+++ algoritmo.js
@@ -0,0 +1,13 @@
+let hora =6;
+if (hora>0 && hora <6) {
+    console.log("Dejame Dormir")
+} 
+else if (hora>5 && hora<12) {
+  console.log("Buenos Dias")  
+}
+else if (hora >11 && hora <20){
+    console.log("Buenas Tardes")
+}
+else if (hora >19 && hora<0){
+    console.log("BuenasNoches")
+}
